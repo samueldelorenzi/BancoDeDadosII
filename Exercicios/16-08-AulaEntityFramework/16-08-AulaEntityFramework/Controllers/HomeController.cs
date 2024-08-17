123c948d85911ccf1b7c75b7f89f462c49757cc7
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-08-16 21:45:05 -0300

    Criação de projeto da primeira aula (16/08/2024)

---
+++ Exercicios/16-08-AulaEntityFramework/16-08-AulaEntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using _16_08_AulaEntityFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _16_08_AulaEntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
