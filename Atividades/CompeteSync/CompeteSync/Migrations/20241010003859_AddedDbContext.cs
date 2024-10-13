1b2917dda2b893186d820f345024802b2a0eff44
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-10-13 20:43:50 -0300

    adicionados ao db context

---
+++ Atividades/CompeteSync/CompeteSync/Migrations/20241010003859_AddedDbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompeteSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WinnerId = table.Column<int>(type: "int", nullable: false),
+                    WinnerPoints = table.Column<int>(type: "int", nullable: false),
+                    LoserId = table.Column<int>(type: "int", nullable: false),
+                    LoserPoints = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamId = table.Column<int>(type: "int", nullable: false),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    EnterDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamGroups", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genders");
+
+            migrationBuilder.DropTable(
+                name: "Results");
+
+            migrationBuilder.DropTable(
+                name: "Stages");
+
+            migrationBuilder.DropTable(
+                name: "TeamGroups");
+        }
+    }
+}
--- Atividades/CompeteSync/CompeteSync/Models/MyDbContext.cs
+++ Atividades/CompeteSync/CompeteSync/Models/MyDbContext.cs
@@ -12,5 +12,9 @@ namespace CompeteSync.Models
         public DbSet<Player> Players { get; set; }
         public DbSet<PlayerTeam> PlayerTeams { get; set; }
         public DbSet<Team> Teams { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<TeamGroup> TeamGroups { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Stage> Stages { get; set; }
     }
 }
